fc0571b56e4c05ec6dc30e8698c4e890a34e18b9
Mark Bolier <markbolier@icloud.com>
2023-01-31 10:38:42 +0100

    feat: convert unique words set to array and show it in list

--- src/components/typeahead-dropdown/TypeaheadDropdown.tsx
+++ src/components/typeahead-dropdown/TypeaheadDropdown.tsx
@@ -1,3 +1,4 @@
+import { Title } from "../header/Header.styled";
 import * as Styled from "./TypeaheadDropdown.styled";
 
 interface TypeaheadDropdownProps {
@@ -8,18 +9,18 @@ interface TypeaheadDropdownProps {
 export const TypeaheadDropdown = ({ data, query }: TypeaheadDropdownProps) => {
   const regex = /\b[^\s]+\b/g;
   const titles = [...data.map((obj: any) => obj.title.toLowerCase())];
-  const allWords = [...titles.map((title) => title.match(regex))];
-  console.log(allWords);
+  const allWords = [].concat(...titles.map((title) => title.match(regex) || []));
+  const uniqueWords = Array.from(new Set(allWords));
 
-  const autocompleteData = data.filter((item: any) =>
-    item.title.toLowerCase().includes(query.toLowerCase()),
-  );
+  console.log(uniqueWords);
+
+  const autocompleteData = uniqueWords.filter((item: any) => item.includes(query.toLowerCase()));
 
   return (
     <Styled.AutocompleteDropdown>
       <Styled.AutocompleteList>
         {autocompleteData.map((item: any) => (
-          <Styled.AutocompleteItem>{item.title}</Styled.AutocompleteItem>
+          <Styled.AutocompleteItem>{item}</Styled.AutocompleteItem>
         ))}
       </Styled.AutocompleteList>
     </Styled.AutocompleteDropdown>
