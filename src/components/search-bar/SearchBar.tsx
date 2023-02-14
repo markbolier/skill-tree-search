4257442906f99e45dd20fddad98e39335a18eeec
Mark Bolier <markbolier@icloud.com>
2023-02-14 16:43:46 +0100

    wip: move logic from Typeahead to SearchBar

--- src/components/search-bar/SearchBar.tsx
+++ src/components/search-bar/SearchBar.tsx
@@ -1,3 +1,5 @@
+import { useRef, useState } from "react";
+
 import { TypeaheadDropdown } from "../typeahead-dropdown";
 import * as Styled from "./SearchBar.styled";
 
@@ -9,10 +11,60 @@ interface SearchBarProps {
 }
 
 export const SearchBar = ({ data, handleInput, query, updateInput }: SearchBarProps) => {
+  const [focusIndex, setFocusIndex] = useState(-1);
+  const [isOpen, setIsOpen] = useState(true);
+  const autoCompleteRef = useRef(null);
+
+  const regex = /\b[^\s]+\b/g;
+  const titles = [...data.map((obj: any) => obj.title.toLowerCase())];
+  const allWords = [].concat(...titles.map((title) => title.match(regex) || []));
+  const uniqueWords = [...new Set(allWords)];
+  const autocompleteData = uniqueWords.filter((item: string) => item.includes(query.toLowerCase()));
+
+  const handleClick = (event: any) => {
+    setIsOpen(!isOpen);
+    updateInput(event.currentTarget.innerText);
+  };
+
+  const handleKeyDown = (event: React.KeyboardEvent<HTMLElement>) => {
+    switch (event.key) {
+      case "ArrowUp":
+        event.preventDefault();
+        if (focusIndex > -1) {
+          setFocusIndex(focusIndex - 1);
+        }
+        break;
+      case "ArrowDown":
+      case "Tab":
+        event.preventDefault();
+        if (focusIndex < autocompleteData.length - 1 && focusIndex < 9) {
+          setFocusIndex(focusIndex + 1);
+        }
+        break;
+      case "Enter":
+        event.preventDefault();
+        const suggestion = autocompleteData[focusIndex];
+        updateInput(suggestion);
+        setFocusIndex(0);
+        setIsOpen(false);
+        break;
+    }
+  };
+
   return (
     <Styled.SearchBarContainer>
       <Styled.Input value={query} onChange={handleInput} placeholder="Search..." type="search" />
-      {query !== "" && <TypeaheadDropdown data={data} query={query} updateInput={updateInput} />}
+      {query !== "" && (
+        <TypeaheadDropdown
+          autoCompleteData={autocompleteData}
+          autoCompleteRef={autoCompleteRef}
+          focusIndex={focusIndex}
+          handleClick={handleClick}
+          handleKeyDown={handleKeyDown}
+          isOpen={isOpen}
+          query={query}
+        />
+      )}
     </Styled.SearchBarContainer>
   );
 };
--- src/components/typeahead-dropdown/TypeaheadDropdown.tsx
+++ src/components/typeahead-dropdown/TypeaheadDropdown.tsx
@@ -1,62 +1,30 @@
-import { useRef, useState } from "react";
-
 import * as Styled from "./TypeaheadDropdown.styled";
 
 interface TypeaheadDropdownProps {
-  data: any;
+  autoCompleteData: any;
+  autoCompleteRef: any;
+  focusIndex: number;
+  handleClick: any;
+  handleKeyDown: any;
+  isOpen: any;
   query: string;
-  updateInput: (event: any) => void;
 }
 
-export const TypeaheadDropdown = ({ data, query, updateInput }: TypeaheadDropdownProps) => {
-  const [focusIndex, setFocusIndex] = useState(-1);
-  const [isOpen, setIsOpen] = useState(true);
-  const autoCompleteRef = useRef(null);
-
-  const regex = /\b[^\s]+\b/g;
-  const titles = [...data.map((obj: any) => obj.title.toLowerCase())];
-  const allWords = [].concat(...titles.map((title) => title.match(regex) || []));
-  const uniqueWords = [...new Set(allWords)];
-  const autocompleteData = uniqueWords.filter((item: string) => item.includes(query.toLowerCase()));
-
-  const handleClick = (
-    event: React.BaseSyntheticEvent<MouseEvent, EventTarget & HTMLElement, EventTarget>,
-  ) => {
-    setIsOpen(!isOpen);
-    updateInput(event.currentTarget.innerText);
-  };
-
-  const handleKeyDown = (event: React.KeyboardEvent<HTMLElement>) => {
-    switch (event.key) {
-      case "ArrowUp":
-        event.preventDefault();
-        if (focusIndex > -1) {
-          setFocusIndex(focusIndex - 1);
-        }
-        break;
-      case "ArrowDown":
-      case "Tab":
-        event.preventDefault();
-        if (focusIndex < autocompleteData.length - 1 && focusIndex < 9) {
-          setFocusIndex(focusIndex + 1);
-        }
-        break;
-      case "Enter":
-        event.preventDefault();
-        const suggestion = autocompleteData[focusIndex];
-        updateInput(suggestion);
-        setFocusIndex(0);
-        setIsOpen(false);
-        break;
-    }
-  };
-
+export const TypeaheadDropdown = ({
+  autoCompleteData,
+  autoCompleteRef,
+  focusIndex,
+  handleClick,
+  handleKeyDown,
+  isOpen,
+  query,
+}: TypeaheadDropdownProps) => {
   return (
     <>
-      {autocompleteData.length > 0 && query.length > 1 && isOpen && (
+      {autoCompleteData.length > 0 && query.length > 1 && isOpen && (
         <Styled.AutocompleteDropdown tabIndex={-1}>
           <Styled.AutocompleteList tabIndex={-1}>
-            {autocompleteData.slice(0, 10).map((item: string, i: number) => (
+            {autoCompleteData.slice(0, 10).map((item: string, i: number) => (
               <Styled.AutocompleteItem
                 focusIndex={focusIndex}
                 key={i}
